c9b376dbd48b0402cfc01dbdd3e9f97bf032e6ef
Wessel <itseagle15@gmail.com>
2026-04-01 11:14:47 +0200

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Frontend-keuzendeel/',
+
   plugins: [
     vue(),
     vueDevTools(),
